b6c1488a26c678f63949d92e7029630c2dea46f3
devonnc <121971591+devonnc@users.noreply.github.com>
2023-12-12 12:43:31 -0800

    1st half

---
+++ Player.gd
@@ -0,0 +1,22 @@
+extends Area2D
+@export var Speed = 400
+
+func _process(delta):
+	var velocity = Vector2.ZERO
+	if Input.is_action_pressed("move_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("move_left"):
+		velocity.x += 1
+	if Input.is_action_pressed("move_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("move_up"):
+		velocity.y += 1
+	if velocity.length > 0:
+
+	position += velocity * delta	
+		
+
+
+
+	
+	
